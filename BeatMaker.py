9c431c4535c90b983d64e0ff5316f5a635e4616e
craja <crajahni@gmail.com>
2022-06-06 09:11:48 -0500

    2nd Commit

--- BeatMaker.py
+++ BeatMaker.py
@@ -4,7 +4,7 @@ import pygame
 from pygame import mixer
 
 
-def draw_grid():
+def draw_grid(clicks):
     left_box = pygame.draw.rect(screen, gray, [0, 0, 200, HEIGHT - 200], 5)
     bottom_box = pygame.draw.rect(screen, gray, [0, HEIGHT - 200, WIDTH, 200], 5)
     boxes = []
@@ -28,9 +28,33 @@ def draw_grid():
     floor_tom_text = label_font.render('Floor Tom', True, white)
     screen.blit(floor_tom_text, (30, 530))
 
-    for i in range(6):
+    for i in range(instruments):
         pygame.draw.line(screen, gray, (0, (i * 100) + 100), (200, (i * 100) + 100), 3)
 
+    for i in range(beats):
+        for j in range(instruments):
+            if clicks[j][i] == -1:
+                color = gray
+            else:
+                color = green
+
+            rect = pygame.draw.rect(screen, color,
+                                    [i * ((WIDTH - 200) // beats) + 205,
+                                     (j * 100) + 5,
+                                     ((WIDTH - 200) // beats) - 10,
+                                     ((HEIGHT - 200) // instruments) - 10], 0, 3)
+
+            pygame.draw.rect(screen, gold, [i * ((WIDTH - 200) // beats) + 200,
+                                            (j * 100),
+                                            ((WIDTH - 200) // beats),
+                                            ((HEIGHT - 200) // instruments)], 5, 5)
+
+            pygame.draw.rect(screen, black, [i * ((WIDTH - 200) // beats) + 200,
+                                             (j * 100),
+                                             ((WIDTH - 200) // beats),
+                                             ((HEIGHT - 200) // instruments)], 2, 5)
+            boxes.append((rect, (i, j)))
+    return boxes
 
 
 pygame.init()
@@ -41,6 +65,8 @@ HEIGHT = 800
 black = (0, 0, 0)
 white = (255, 255, 255)
 gray = (128, 128, 128)
+green = (0, 255, 0)
+gold = (212, 175, 55)
 
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption('Beat Maker')
@@ -48,16 +74,25 @@ label_font = pygame.font.Font(None, 32)
 
 fps = 60
 timer = pygame.time.Clock()
+beats = 8
+instruments = 6
+boxes = []
+clicked = [[-1 for _ in range(beats)] for _ in range(instruments)]
 
 run = True
 while run:
     timer.tick(fps)
     screen.fill(black)
-    draw_grid()
+    boxes = draw_grid(clicked)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for i in range(len(boxes)):
+                if boxes[i][0].collidepoint(event.pos):
+                    coords = boxes[i][1]
+                    clicked[coords[1]][coords[0]] *= -1
 
     pygame.display.flip()
 pygame.quit()
